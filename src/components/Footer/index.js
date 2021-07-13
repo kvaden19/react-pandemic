a4698dce0e6b336ec266c8da7ffcaf534b40db75
Kelly Vaden <klvaden@gmail.com>
2021-07-12 21:23:04 -0400

    Component layout for Header and Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='w-screen'>
+        <p className='footer fixed bottom-0 w-full m-2 p-2 text-center text-xs text-white border-white border-2'>Copyright 2021 Kelly L Vaden</p>
+    </div>
+    );
+}
+
+export default Footer;
